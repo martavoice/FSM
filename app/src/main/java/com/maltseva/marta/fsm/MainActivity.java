5b6f8c584a31cc53a923f2934ff987daa607a09d
Marta <mmaltseva@perpet.io>
2018-02-21 22:28:21 +0200

    add ui

--- app/src/main/java/com/maltseva/marta/fsm/MainActivity.java
+++ app/src/main/java/com/maltseva/marta/fsm/MainActivity.java
@@ -2,12 +2,48 @@ package com.maltseva.marta.fsm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.indicator)
+    ImageView mIndicator;
+    @BindView(R.id.state)
+    TextView mState;
+    @BindView(R.id.lock)
+    Button mLock;
+    @BindView(R.id.unlock)
+    Button mUnLock;
+    @BindView(R.id.lockx2)
+    Button mLockx2;
+    @BindView(R.id.unlockx2)
+    Button mUnlockx2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.lock, R.id.lockx2, R.id.unlock, R.id.unlockx2})
+    void btnClicks(View view) {
+        switch (view.getId()) {
+            case R.id.lock:
+                break;
+            case R.id.unlock:
+                break;
+            case R.id.lockx2:
+                break;
+            case R.id.unlockx2:
+                break;
+        }
     }
 }
